061f20e35905c4ba8cc5136652472ef2ac52e3dd
Naushad Ali Khan <naushadkhan@Naushads-Air.lan>
2026-01-08 23:58:43 +0530

    Initial UIKit project setup

---
+++ PremierLeagueChantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PremierLeagueChantApp
+//
+//  Created by Naushad Ali Khan on 08/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
